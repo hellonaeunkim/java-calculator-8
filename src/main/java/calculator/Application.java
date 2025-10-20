a3fbe19503412a55e2ba564343086a6bb753be03
hellonaeunkim <hellonaeunkim@gmail.com>
2025-10-20 23:53:31 +0900

    fix : 테스트 코드 오류 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,27 +3,22 @@ package calculator;
 public class Application {
 
     public static void main(String[] args) {
-        try {
-            // 사용자 입력 받기
-            String input = Input.input();
+        // 사용자 입력 받기
+        String input = Input.input();
 
-            // 구분자 찾기
-            String delimiter = StringSplitter.findDelimiters(input);
+        // 구분자 찾기
+        String delimiter = StringSplitter.findDelimiters(input);
 
-            // 문자열 분리
-            String[] numbers = StringSplitter.split(input, delimiter);
+        // 문자열 분리
+        String[] numbers = StringSplitter.split(input, delimiter);
 
-            // 숫자 파싱
-            int[] parsedNumbers = NumberParser.parse(numbers);
+        // 숫자 파싱
+        int[] parsedNumbers = NumberParser.parse(numbers);
 
-            // 숫자들의 합 계산
-            int sumOfNumbers = Calculator.sum(parsedNumbers);
+        // 숫자들의 합 계산
+        int sumOfNumbers = Calculator.sum(parsedNumbers);
 
-            // 계산 결과 출력
-            Output.printResult(sumOfNumbers);
-
-        } catch (IllegalArgumentException e) {
-            System.out.println("에러: " + e.getMessage());
-        }
+        // 계산 결과 출력
+        Output.printResult(sumOfNumbers);
     }
 }
